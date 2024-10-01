14a156458a3b0631adb934a765ce839b65487dc6
Clement Deville <cdeville@made-f0Br7s5.clusters.42paris.fr>
2024-10-01 12:15:14 +0200

    Patch de l'ex06, utilisation du switch statement

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 18:17:30 by cdeville          #+#    #+#             */
-/*   Updated: 2024/10/01 11:56:49 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/10/01 12:13:57 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,23 @@ void	Harl::error(void)
 	std::cout << ERROR_MSG << std::endl;
 }
 
-void	Harl::complain( std::string level )
+// void	Harl::complain( std::string level )
+// {
+// 	std::string	level_name[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+// 	for (int i = 0; i < 4; i++)
+// 	{
+// 		if (level.compare(level_name[i]) == 0)
+// 		{
+// 			for (int j = i; j < 4; j++)
+// 				(this->*ptr_tab[j])();
+// 			return ;
+// 		}
+// 	}
+// 	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+// }
+
+static int	find_index(std::string level)
 {
 	std::string	level_name[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
@@ -54,10 +70,33 @@ void	Harl::complain( std::string level )
 	{
 		if (level.compare(level_name[i]) == 0)
 		{
-			for (int j = i; j < 4; j++)
-				(this->*ptr_tab[j])();
-			return ;
+			return (i);
 		}
 	}
-	std::cerr << "No matching complain" << std::endl;
+	return (-1);
+}
+
+void	Harl::complain( std::string level )
+{
+	int	i;
+
+	i = 0;
+	switch ( find_index(level) )
+	{
+	case -1:
+	{
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+		return ;
+	}
+	case 0:
+		(this->*ptr_tab[0])();
+	case 1:
+		(this->*ptr_tab[1])();
+	case 2:
+		(this->*ptr_tab[2])();
+	case 3:
+		(this->*ptr_tab[3])();
+	default:
+		break;
+	}
 }
--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,16 +6,19 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 18:16:13 by cdeville          #+#    #+#             */
-/*   Updated: 2024/10/01 11:28:50 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/10/01 12:03:00 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
 	Harl	test;
 
-	test.complain("DEBUG");
+	if (argc != 2)
+		return (std::cerr << "Error, programm needs one argument."
+			<< std::endl, 1);
+	test.complain(argv[1]);
 	return (0);
 }
